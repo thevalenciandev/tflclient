a96c3370f5638cc1778337bba8afa70fec5cf840
TVD <thevalenciandev@gmail.com>
2020-12-27 18:14:46 +0000

    Add disruption details to disrupted lines in tube_status example

--- examples/tube_status.py
+++ examples/tube_status.py
@@ -8,4 +8,9 @@ tube_status = tflclient.get_status('tube')
 print('Tube status:')
 for line in tube_status:
     statuses = [st.statusSeverityDescription for st in line.lineStatuses]
+    disruption_details = {'detail': st.reason for st in line.lineStatuses
+                          if st.statusSeverity != 10}  # 10 is Good Service
     print(line.id, ':', "/".join(statuses))
+    if disruption_details:
+        [print(v) for v in disruption_details.values()]
+    print()
